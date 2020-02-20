5a806a2b99009f7607bed3e6246bee73960a760c
Spencer Harmon <spencerharmon.91@gmail.com>
2020-02-20 02:53:24 -0600

    initial commit

---
+++ src/emm_cell_viewer/app/parse.py
@@ -0,0 +1,12 @@
+import os
+from mfm_emm_graph.grid import Grid
+
+directory = os.environ.get("EMM_GD_DIR")
+print(os.environ)
+print(directory)
+epochs = {}
+for filename in os.listdir(directory):
+    print(filename)
+    if filename.endswith('.save'):
+        epochs.update({filename.split('.')[0]: Grid(f'{directory}/{filename}')})
+
---
+++ src/emm_cell_viewer/app/routes.py
@@ -0,0 +1,25 @@
+from . import app
+from .parse import epochs
+from flask import render_template
+import io
+from contextlib import redirect_stdout
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', epochs=epochs)
+
+@app.route('/epoch/<epoch>')
+def epoch(epoch):
+    grid = epochs[epoch]
+    with io.StringIO() as buf, redirect_stdout(buf):
+        grid.print_grid_ascii()
+        grid_ascii = buf.getvalue()
+    return render_template('epoch.html', grid=grid, grid_ascii=grid_ascii, epoch=epoch)
+
+@app.route('/epoch/<epoch>/cell/<identifier>')
+def cell(epoch, identifier):
+    for c in epochs[epoch].cells:
+        if c.identifier.data_members["id"] == identifier:
+            cell = c
+    return render_template('cell.html', cell=cell)

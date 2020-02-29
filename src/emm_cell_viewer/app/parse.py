629c539fbbe859aab3d304574ef70331f306e236
Spencer Harmon <spencerharmon.91@gmail.com>
2020-02-28 23:38:49 -0600

    Delete extraneous print statements. File extension changed to json.

--- src/emm_cell_viewer/app/parse.py
+++ src/emm_cell_viewer/app/parse.py
@@ -2,11 +2,9 @@ import os
 from mfm_emm_graph.grid import Grid
 
 directory = os.environ.get("EMM_GD_DIR")
-print(os.environ)
-print(directory)
 epochs = {}
 for filename in os.listdir(directory):
     print(filename)
-    if filename.endswith('.save'):
+    if filename.endswith('.json'):
         epochs.update({filename.split('.')[0].split('-')[-1]: Grid(f'{directory}/{filename}')})
 
